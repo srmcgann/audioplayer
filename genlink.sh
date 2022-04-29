ac296a5dc67fd65c7c13c80f2671fd49ba1f1c0c
Scott McGann <whitehotrobot@gmail.com>
2022-04-29 11:13:14 -0700

    initial commit

---
+++ genlink.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s /var/www/html/whitehotrobot_assets/audio /var/www/html/audiocloud.dweet.net/dist/audio
+ln -s /var/www/html/whitehotrobot_assets/audio /var/www/html/audiocloud.dweet.net/public/audio
+
